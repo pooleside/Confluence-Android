788917e1d01799c52eb097ed9ff519b37290709e
pooleside <lpoole26@gmail.com>
2015-10-28 17:00:16 -0700

    Initial commit

---
+++ app/src/main/java/com/epicodus/confluence/MainActivity.java
@@ -0,0 +1,47 @@
+package com.epicodus.confluence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        ImageView bridgeImage = (ImageView)findViewById(R.id.bridgeImage);
+
+        bridgeImage.setImageResource(R.drawable.land_10);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
