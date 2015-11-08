8b7411fba232d871aec99736e45228299717cea6
pooleside <lpoole26@gmail.com>
2015-11-07 22:32:48 -0800

    "user can register"

--- app/src/main/java/com/epicodus/confluence/ui/RegisterActivity.java
+++ app/src/main/java/com/epicodus/confluence/ui/RegisterActivity.java
@@ -1,58 +1,50 @@
 package com.epicodus.confluence.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 
 import com.epicodus.confluence.R;
 
+public class RegisterActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button mLandBridgeButton;
-
-    public void clickFunction(View view) {
-        ImageView bridgeImage = (ImageView)findViewById(R.id.bridgeImage);
-
-        bridgeImage.setImageResource(R.drawable.land_10);
-
-    }
+    private EditText mNameEdit;
+    private Button mLoginButton;
+    private SharedPreferences mPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
-        mLandBridgeButton = (Button) findViewById(R.id.landBridgeButton);
+        mNameEdit = (EditText) findViewById(R.id.nameText);
+        mLoginButton = (Button) findViewById(R.id.registerButton);
+        mPreferences = getApplicationContext().getSharedPreferences("twitter", Context.MODE_PRIVATE);
 
-        mLandBridgeButton.setOnClickListener(new View.OnClickListener() {
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, LandBridgeActivity.class);
+                String name = mNameEdit.getText().toString();
+                SharedPreferences.Editor editor = mPreferences.edit();
+                editor.putString("username", name);
+                editor.commit();
+                Intent intent= new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
-        if(!isRegistered()) {
-            Intent intent = new Intent(this, RegisterActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    private boolean isRegistered() {
-        return false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
     }
 
